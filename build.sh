0460ba309c173cefbdfe18bf11487caa28ca179a
long.mai <long.mai@realtimeatwork.com>
2026-01-12 11:00:43 +0100

    add some needed commands

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# start a new git repository
 git init
 git add .
 git commit -m "Initial torch2transformer scaffold"
@@ -9,4 +10,8 @@ git remote add origin git@github.com:longmaisg/torch2transformer.git
 git remote -v
 
 git push -u origin main
+
+
+uv pip install -e .
+python -c "from torch2transformer import wrap_model; print(wrap_model)"
 
